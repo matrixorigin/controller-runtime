a9f059e9668ef36c0d54ded6ed09e60b5e2e308e
wanglei4687 <wanglei4687@gmail.com>
2022-11-30 13:45:40 +0800

    update
    Signed-off-by: wanglei4687 <wanglei4687@gmail.com>

--- pkg/fake/client.go
+++ pkg/fake/client.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -53,7 +53,7 @@ func (c *Client) RESTMapper() meta.RESTMapper {
 	return c.Client.RESTMapper()
 }
 
-func (c *Client) Get(ctx context.Context, key client.ObjectKey, obj client.Object) error {
+func (c *Client) Get(ctx context.Context, key client.ObjectKey, obj client.Object, opts ...client.GetOption) error {
 	if c.MockGet != nil {
 		return c.MockGet(ctx, key, obj)
 	}
